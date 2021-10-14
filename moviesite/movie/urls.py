a6797ff0c701254ab45af66ff16ad39f4def92e6
Codernob <arnob319@gmail.com>
2021-10-14 23:25:02 +0600

    added layout and movies page

--- moviesite/movie/urls.py
+++ moviesite/movie/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name="home"),
+    path('movies.html',views.movies,name="movies")
 ]
--- moviesite/movie/views.py
+++ moviesite/movie/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from .models import Movie,Cast,Director
 # Create your views here.
 
 def home(request):
     return render(request,'home.html',{})
+
+def movies(request):
+    return render(request,'movies.html',{
+        "movies":Movie.objects.all()
+    })
