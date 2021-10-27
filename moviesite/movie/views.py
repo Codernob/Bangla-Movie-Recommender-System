560f935ee25862bdc36da90e541113a28e960a77
Nakiba20 <19101039@uap-bd.edu>
2021-10-27 18:08:40 +0600

    views.py updated

--- moviesite/movie/views.py
+++ moviesite/movie/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .models import Movie,Cast,Director,Genre
+from django.shortcuts import redirect,HttpResponse ,render
+from django.contrib import messages
+from django.contrib.auth.models import User,auth
+from .models import Genders,Movie,Cast,Director,Genre
 # Create your views here.
 
 def home(request):
@@ -37,3 +39,37 @@ def search(request):
     searched = request.POST['searched']
     movie_re = Movie.objects.filter(title__contains=searched)
     return render(request,'searchresult.html', {'movie_re':movie_re})
+
+def login(request):  
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        
+        user=auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,'Invalid Credentials')
+            return redirect('login')        
+    else:
+        return render(request,'layout.html')     
+
+def signup(request):
+    if request.method== 'POST':
+        username=request.POST['username']
+        email=request.POST['email']
+        date_of_birth=request.POST['date_of_birth']
+        #gender=request.POST['gender']
+        password1=request.POST['password1']
+        password2=request.POST['password2']
+
+        if password1==password2:
+            user=User.objects.create_user(username=username,email=email,date_of_birth=date_of_birth,pasasword=password1)
+            user.save();
+            return redirect('login') 
+        #print('User created')
+        return redirect('/')
+    # else:
+    return render(request,'layout.html')     
